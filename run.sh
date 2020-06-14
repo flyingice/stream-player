5bc14938ec140ac25948cf50f32a1c3096214c44
flyingice <flyingice.eth@outlook.com>
2020-06-14 10:41:10 +0200

    Support for funky server startup & update doc

--- run.sh
+++ run.sh
@@ -3,13 +3,14 @@
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source "$ROOT/common.sh"
 
+SERVER_TYPE=${1:-"Server"}
 SERVER_NAME=localhost
 SERVER_PORT=1554
 MEDIA="$ROOT/data/movie.mjpeg"
 
 # start streaming server
 echo "Starting server ..."
-java -classpath "$TARGET" Server "$SERVER_PORT" &
+java -classpath "$TARGET" "$SERVER_TYPE" "$SERVER_PORT" &
 echo "Server started."
 
 # wait for the sever to fully start before running the client
